da30c1aca67b792ad04586b01ff7faf1f4ea02a7
viktor <sterilework@gmail.com>
2025-01-26 14:47:32 +0300

    docs: add example 02, rename folder, move code fragment from 01 to 02

--- samples/01_linq/01_Basics/Program.cs
+++ samples/01_linq/01_Basics/Program.cs
@@ -43,13 +43,6 @@
                 Console.WriteLine(s);
 
             Console.WriteLine();
-
-            // Преобразовать массив строк в массив типа int и отсортировать по возрастанию
-            string[] numberStrings = { "40", "2012", "176", "5" };
-
-            int[] nums = numberStrings.Select(s => Int32.Parse(s)).OrderBy(s => s).ToArray();
-
-            foreach (int s in nums) Console.WriteLine(s);
         }
     }
 }
---
+++ samples/01_linq/02_LambdaExp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _02_LambdaExp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Преобразовать массив строк в массив типа int и отсортировать по возрастанию
+            string[] numberStrings = { "40", "2012", "176", "5" };
+
+            int[] nums = numberStrings.Select(s => Int32.Parse(s)).OrderBy(s => s).ToArray();
+
+            foreach (int s in nums) Console.WriteLine(s);
+        }
+    }
+}
