2119a38c62476b95566d7c868bb5b222a6d715b2
viktor <sterilework@gmail.com>
2025-02-12 14:34:08 +0300

    docs: add academy app sample

---
+++ samples/03_AcademyApp/ApplicationData/AppConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademyApp.ApplicationData
+{
+    internal class AppConnect
+    {
+        public static AcademyEntities modelOdb;
+    }
+}
