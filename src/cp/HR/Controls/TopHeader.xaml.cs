c8ec45f9c5d577764752028bc9d5cc9be4ffaffc
viktor <sterilework@gmail.com>
2025-05-05 22:23:35 +0300

    feat: handle navigation state for top menu

--- src/cp/HR/Controls/TopHeader.xaml.cs
+++ src/cp/HR/Controls/TopHeader.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using HR.Pages;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,75 @@ using System.Windows.Shapes;
 
 namespace HR.Controls
 {
+    public class PageToIsEnabledConverter : IValueConverter
+    {
+        // value - имя текущей страницы (string)
+        // parameter - имя страницы для пункта меню (string)
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string currentPage = value as string;
+            string menuPage = parameter as string;
+
+            if (string.IsNullOrEmpty(currentPage) || string.IsNullOrEmpty(menuPage))
+                return true; // пункт меню доступен по умолчанию
+
+            // Если текущая страница совпадает с пунктом меню - пункт не доступен (IsEnabled = false)
+            return !string.Equals(currentPage, menuPage, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class PageToIsCheckedConverter : IValueConverter
+    {
+        // value - текущая страница (string)
+        // parameter - строка с перечнем страниц, связанных с пунктом меню, разделенных запятыми
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string currentPage = value as string;
+            string pagesParam = parameter as string;
+
+            if (string.IsNullOrEmpty(currentPage) || string.IsNullOrEmpty(pagesParam))
+                return true;
+
+            var pages = pagesParam.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                                  .Select(p => p.Trim());
+
+            return pages.Contains(currentPage, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
     /// <summary>
     /// Interaction logic for TopHeader.xaml
     /// </summary>
     public partial class TopHeader : UserControl
     {
+        public static readonly DependencyProperty CurrentPageProperty =
+        DependencyProperty.Register(nameof(CurrentPage), typeof(string), typeof(TopHeader), new PropertyMetadata(null));
+
+        public string CurrentPage
+        {
+            get => (string)GetValue(CurrentPageProperty);
+            set => SetValue(CurrentPageProperty, value);
+        }
         public TopHeader()
         {
             InitializeComponent();
+            Loaded += Page_Loaded;
+        }
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            MainWindow.frame.Navigated += Nav_Navigated;
+        }
+        private void Nav_Navigated(object sender, NavigationEventArgs e)
+        {
+            CurrentPage = e.Content.GetType().Name;
         }
 
         private void SignInItem_Click(object sender, RoutedEventArgs e)
