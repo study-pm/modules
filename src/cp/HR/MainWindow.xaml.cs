0e263baac36ddceab0512871e9397e2d87a49e32
viktor <sterilework@gmail.com>
2025-05-08 12:26:44 +0300

    feat: add adaptive design features

--- src/cp/HR/MainWindow.xaml.cs
+++ src/cp/HR/MainWindow.xaml.cs
@@ -23,10 +23,56 @@ namespace HR
     public partial class MainWindow : Window
     {
         public static Frame frame {  get; set; }
+        private const double Breakpoint1Width = 1080;
+        private const double Breakpoint2Width = 720;
+        private const double MinimalWidth = 220;
+        private const double NormalWidth = 300;
+        private const double NormalMargin = 30;
         public MainWindow()
         {
             InitializeComponent();
             MainWindow.frame = mainFrame;
+            this.SizeChanged += MainWindow_SizeChanged;
+            UpdateVisibility(this.ActualWidth);
+        }
+        private void MainWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            UpdateVisibility(e.NewSize.Width);
+        }
+        private void SetRightSide(bool IsVisible)
+        {
+            RightAside.Visibility = IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            RightSplitter.Visibility = IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            MainLayoutContainer.ColumnDefinitions[4].Width = IsVisible ? new GridLength(NormalWidth, GridUnitType.Pixel) : new GridLength(0);
+            MainLayoutContainer.ColumnDefinitions[4].MinWidth = IsVisible ? MinimalWidth : 0;
+        }
+        private void SetLeftSide(bool IsVisible)
+        {
+            LeftAside.Visibility = IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            LeftSplitter.Visibility = IsVisible ? Visibility.Visible : Visibility.Collapsed;
+            MainLayoutContainer.ColumnDefinitions[0].Width = IsVisible ? new GridLength(NormalWidth, GridUnitType.Pixel) : new GridLength(0);
+            MainLayoutContainer.ColumnDefinitions[0].MinWidth = IsVisible ? MinimalWidth : 0;
+        }
+        private void UpdateVisibility(double width)
+        {
+            if (width < Breakpoint2Width)
+            {
+                SetLeftSide(false);
+                SetRightSide(false);
+                Main.Margin = new Thickness(NormalMargin, 0, NormalMargin, 0);
+            }
+            else if (width < Breakpoint1Width)
+            {
+                SetLeftSide(true);
+                SetRightSide(false);
+                Main.Margin = new Thickness(0, 0, NormalMargin, 0);
+            }
+            else
+            {
+                SetLeftSide(true);
+                SetRightSide(true);
+                Main.Margin = new Thickness(0);
+            }
         }
     }
 }
