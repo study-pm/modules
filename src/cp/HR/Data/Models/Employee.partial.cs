f5718cc293b034441e274e5a06e15f4350ca8283
viktor <sterilework@gmail.com>
2025-05-15 17:08:27 +0300

    feat: add partial employee class with experience calculation

---
+++ src/cp/HR/Data/Models/Employee.partial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Data.Models
+{
+    public partial class Employee
+    {
+        /// <summary>
+        /// Returns work experience in full years calculated from CareerStart until the current date
+        /// Returns 0 if CareerStart is unset
+        /// </summary>
+        public int Experience
+        {
+            get
+            {
+                if (!CareerStart.HasValue)
+                    return 0;
+
+                DateTime start = CareerStart.Value;
+                DateTime now = DateTime.Today;
+
+                int years = now.Year - start.Year;
+
+                // Если текущая дата раньше даты начала карьеры в этом году, уменьшаем на 1 год
+                if (now.Month < start.Month || (now.Month == start.Month && now.Day < start.Day))
+                    years--;
+
+                return years >= 0 ? years : 0;
+            }
+        }
+    }
+}
