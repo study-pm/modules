31d395b030755795d4fa671ad636021c41df56a4
viktor <sterilework@gmail.com>
2025-05-15 14:28:49 +0300

    feat: add employee model

--- src/cp/HR/Models/Employee.cs
+++ src/cp/HR/Models/Employee.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace HR.Models
 {
@@ -24,6 +25,31 @@ namespace HR.Models
                 if (CareerStart.Date > today.AddYears(-years)) years--;
                 return years;
             }
+        }
+        public static List<Employee> LoadFromXml(string xml)
+        {
+            XDocument doc = XDocument.Parse(xml);
+            var employees = doc.Descendants("row")
+                .Select(e => new Employee
+                {
+                    // Если id пустой, можно присвоить 0 или сгенерировать
+                    Id = int.TryParse((string)e.Element("id"), out int id) ? id : 0,
+                    Surname = (string)e.Element("surname"),
+                    GivenName = (string)e.Element("given_name"),
+                    Patronymic = (string)e.Element("patronymic"),
+                    Gender = ((string)e.Element("sex")) == "m" ? false : true, // false = муж, true = жен
+                    CareerStart = ParseYear((string)e.Element("career_start"))
+                })
+                .ToList();
+            return employees;
+        }
+
+        // Парсинг года в DateTime
+        private static DateTime ParseYear(string year)
+        {
+            if (int.TryParse(year, out int y))
+                return new DateTime(y, 1, 1);
+            return DateTime.MinValue;
         }
     }
 }
