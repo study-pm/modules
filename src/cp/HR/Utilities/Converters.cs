de398e83b0267e420ee66bb14ad3a9eb2e777900
viktor <sterilework@gmail.com>
2025-05-15 23:52:57 +0300

    feat: add column numeration

--- src/cp/HR/Utilities/Converters.cs
+++ src/cp/HR/Utilities/Converters.cs
@@ -9,6 +9,22 @@ using System.Windows.Data;
 
 namespace HR.Utilities
 {
+    public class AddOneConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int index)
+            {
+                return (index + 1).ToString();
+            }
+            return "0";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
     public class InverseBooleanConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
