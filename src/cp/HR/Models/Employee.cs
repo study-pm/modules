764c499857813e63a1bdb17152c5d2c79a558b41
viktor <sterilework@gmail.com>
2025-05-15 09:28:17 +0300

    feat: add employee model

---
+++ src/cp/HR/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string GivenName { get; set; }
+        public string Patronymic { get; set; }
+        public bool Gender { get; set; }
+        public DateTime CareerStart { get; set; }
+        // Вычисляемый стаж в годах
+        public int Experience
+        {
+            get
+            {
+                var today = DateTime.Today;
+                int years = today.Year - CareerStart.Year;
+                if (CareerStart.Date > today.AddYears(-years)) years--;
+                return years;
+            }
+        }
+    }
+}
