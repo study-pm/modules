c30d7748d57b4340285c6d13b069668f5ac4ac0b
viktor <sterilework@gmail.com>
2025-05-15 09:27:48 +0300

    feat: add request service

--- src/cp/HR/Models/User.cs
+++ src/cp/HR/Models/User.cs
@@ -16,5 +16,8 @@ namespace HR.Models
 
         public int Id { get; set; }
         public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int RoleId { get; set; }
     }
 }
---
+++ src/cp/HR/Services/Request.cs
@@ -0,0 +1,24 @@
+﻿using HR.Models;
+using HR.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Services
+{
+    internal static class Request
+    {
+        public static async Task<List<Employee>> GetEmployees()
+        {
+            // Mock checking user existences from DB
+            await Utils.MockAsync(1000);
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { Id = 1, Surname = "Карягин", GivenName = "Сергей", Patronymic = "Николаевич", Gender = false, CareerStart = new DateTime(2012) }
+            };
+            return employees;
+        }
+    }
+}
