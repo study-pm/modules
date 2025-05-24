f42bb98ebc812a3d44bd168d59cd5bf7a38ad3fe
viktor <sterilework@gmail.com>
2025-05-24 14:43:33 +0300

    feat: add string to bool converter

--- src/cp/HR/Utilities/Converters.cs
+++ src/cp/HR/Utilities/Converters.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Data;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
@@ -241,6 +242,18 @@ namespace HR.Utilities
                 return false;
 
             return string.Equals(currentPage, pagesParam, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value as string);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
