5242f8bbe046ca343f4544bdb9854a95c54832dd
viktor <sterilework@gmail.com>
2025-05-24 10:09:56 +0300

    feat: add createbitmapimage utility method

--- src/cp/HR/Utilities/Utils.cs
+++ src/cp/HR/Utilities/Utils.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace HR.Utilities
 {
     internal static class Utils
     {
+        public static BitmapImage CreateBitmapImage(string uriString)
+        {
+            try
+            {
+                var uri = new Uri(uriString, UriKind.Absolute);
+                var bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = uri;
+                bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                bitmap.EndInit();
+                bitmap.Freeze();
+                return bitmap;
+            }
+            catch
+            {
+                // Return null or a default image if loading fails
+                return null;
+            }
+        }
         public static Task<bool> MockAsync(int delay, bool isSuccess = true)
         {
             var tcs = new TaskCompletionSource<bool>();
