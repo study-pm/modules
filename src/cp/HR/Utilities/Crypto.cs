81f6d4415cff4e7d3aacf761b00e78fd2a8f4a1a
viktor <sterilework@gmail.com>
2025-05-27 15:50:11 +0300

    feat: add crypto class

---
+++ src/cp/HR/Utilities/Crypto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Utilities
+{
+    internal class Crypto
+    {
+        /// <summary>
+        /// Generates random salt
+        /// </summary>
+        /// <param name="size">The size of the salt in bytes (default is 16 bytes)</param>
+        /// <returns>Randomly generated salt as a byte array</returns>
+        public static byte[] GenerateSalt(int size = 16)
+        {
+            byte[] salt = new byte[size];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(salt);
+            }
+            return salt;
+        }
+        /// <summary>
+        /// Gets hash for password and salt combination
+        /// </summary>
+        /// <param name="password">The password string to hash</param>
+        /// <param name="salt">The salt byte array to combine with the password</param>
+        /// <returns>Hashed password as a byte array of length 32 bytes</returns>
+        public static byte[] HashPassword(string password, byte[] salt)
+        {
+            using (var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 10000))
+            {
+                return pbkdf2.GetBytes(32); // 32 bytes — hash length
+            }
+        }
+    }
+}
