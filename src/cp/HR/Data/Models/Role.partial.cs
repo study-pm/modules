d02db4b3171bc45e1599bced8a861cbaec05bbfd
viktor <sterilework@gmail.com>
2025-06-03 12:41:56 +0300

    feat: add role partial class extension

---
+++ src/cp/HR/Data/Models/Role.partial.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Data.Models
+{
+    public partial class Role
+    {
+        public string Name
+        {
+            get
+            {
+                switch (Id)
+                {
+                    case 1:
+                        return "Администратор";
+                    case 2:
+                        return "Сопровождающий";
+                    case 3:
+                        return "Редактор";
+                    case 4:
+                        return "Пользователь";
+                    default:
+                        return Title;
+                }
+            }
+        }
+    }
+}
