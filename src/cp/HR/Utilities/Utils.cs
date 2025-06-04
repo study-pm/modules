bb768909f38a03abd0bc464d69ec0b764c68acae
viktor <sterilework@gmail.com>
2025-06-05 00:03:46 +0300

    feat: add getqrcode method

--- src/cp/HR/Utilities/Utils.cs
+++ src/cp/HR/Utilities/Utils.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HR.Services;
+using QRCoder;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -121,6 +123,25 @@ namespace HR.Utilities
             timer.Start();
 
             return tcs.Task;
+        }
+        /// <summary>
+        /// Generates a QR code image as a byte array representing the OTP Auth URI for a given secret and user login.
+        /// </summary>
+        /// <param name="secret">The secret key used for generating the OTP Auth URI.</param>
+        /// <param name="login">The user login or identifier to include in the OTP Auth URI.</param>
+        /// <returns>A byte array containing the PNG image data of the generated QR code.</returns>
+        /// <remarks>
+        /// The generated QR code encodes the OTP Auth URI, which can be scanned by authenticator apps to set up time-based one-time password (TOTP) authentication.
+        /// </remarks>
+        public static byte[] GetQrCode(byte[] secret, string login)
+        {
+            // Generate OTP Auth URI
+            string pathUrl = Crypto.GetOtpAuthUri(secret, login);
+            // Generate QR code
+            var qrGenerator = new QRCodeGenerator();
+            var qrCodeData = qrGenerator.CreateQrCode(pathUrl, QRCodeGenerator.ECCLevel.Q);
+            var qrCode = new PngByteQRCode(qrCodeData);
+            return qrCode.GetGraphic(20);
         }
     }
 }
