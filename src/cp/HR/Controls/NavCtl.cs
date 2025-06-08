b8634ee432373bbced2c4f3158c5752f327c8513
viktor <sterilework@gmail.com>
2025-06-08 03:19:26 +0300

    feat: move menu view model to main window, sync aside and top header

--- src/cp/HR/Controls/NavCtl.cs
+++ src/cp/HR/Controls/NavCtl.cs
@@ -21,22 +21,20 @@ namespace HR.Controls
     {
         public static readonly DependencyProperty ParameterProperty =
             DependencyProperty.Register("Parameter", typeof(object), typeof(NavigationData));
-
         public object Parameter
         {
             get => GetValue(ParameterProperty);
             set => SetValue(ParameterProperty, value);
         }
 
-        // Остальные свойства
         public static readonly DependencyProperty UriProperty =
             DependencyProperty.Register("Uri", typeof(string), typeof(NavigationData));
-
         public string Uri
         {
             get => (string)GetValue(UriProperty);
             set => SetValue(UriProperty, value);
         }
+
         public string Name { get; set; }
     }
     public abstract class NavCtl : UserControl, INotifyPropertyChanged
@@ -47,90 +45,26 @@ namespace HR.Controls
 
         public static readonly DependencyProperty CurrentPageProperty =
             DependencyProperty.Register(nameof(CurrentPage), typeof(string), typeof(NavCtl), new PropertyMetadata(null));
-
-        public static readonly DependencyProperty InProgressProp =
-            DependencyProperty.Register(nameof(IsInProgress), typeof(bool), typeof(NavCtl), new PropertyMetadata(false));
-
-        public bool IsInProgress
-        {
-            get { return (bool)GetValue(InProgressProp); }
-            set { SetValue(InProgressProp, value); }
-        }
         public string CurrentPage
         {
             get => (string)GetValue(CurrentPageProperty);
             set => SetValue(CurrentPageProperty, value);
         }
+
         public static readonly DependencyProperty PageParamProperty =
             DependencyProperty.Register(nameof(PageParam), typeof(string), typeof(NavCtl), new PropertyMetadata(null));
-
         public string PageParam
         {
             get => (string)GetValue(PageParamProperty);
             set => SetValue(PageParamProperty, value);
         }
-        public ObservableCollection<MenuFilter> Filters { get; } = new ObservableCollection<MenuFilter>()
-        {
-            new MenuFilter()
-            {
-                Icon = (Geometry)Application.Current.FindResource("UsersSolidPath"),
-                Name = "Виды деятельности",
-                Title = "Сотрудники",
-                PageUri = "Pages/StaffPg.xaml",
-                Values = new ObservableCollection<FilterValue>
-                {
-                    new FilterValue { Id = 1, Title = "Администрация" },
-                    new FilterValue { Id = 2, Title = "Учителя" },
-                    new FilterValue { Id = 3, Title = "Преподаватели" },
-                    new FilterValue { Id = 4, Title = "Воспитатели" },
-                }
-            },
-            new MenuFilter()
-            {
-                Icon = (Geometry)Application.Current.FindResource("SitemapSolidPath"),
-                Name = "Структурные подразделения",
-                Title = "Подразделения",
-                PageUri = "Pages/StaffPg.xaml",
-                Values = new ObservableCollection<FilterValue>() // initialize empty collection
-            },
-            new MenuFilter()
-            {
-                Icon = (Geometry)Application.Current.FindResource("BookOpenSolidPath"),
-                Name = "Учебные предметы",
-                Title = "Предметы",
-                PageUri = "Pages/StaffPg.xaml",
-                Values = new ObservableCollection<FilterValue>() // initialize empty collection
-            },
-            new MenuFilter()
-            {
-                Icon = (Geometry)Application.Current.FindResource("BookOpenSolidPath"),
-                Name = "Классное руководство",
-                Title = "Классы",
-                PageUri = "Pages/ClassesPg.xaml",
-                Values = new ObservableCollection<FilterValue>
-                {
-                    new FilterValue { Id = 1, Title = "1 параллель" },
-                    new FilterValue { Id = 2, Title = "2 параллель" },
-                    new FilterValue { Id = 3, Title = "3 пареллель" },
-                    new FilterValue { Id = 4, Title = "4 параллель" },
-                    new FilterValue { Id = 5, Title = "5 параллель" },
-                    new FilterValue { Id = 6, Title = "6 параллель" },
-                    new FilterValue { Id = 7, Title = "7 параллель" },
-                    new FilterValue { Id = 8, Title = "8 параллель" },
-                    new FilterValue { Id = 9, Title = "9 параллель" },
-                    new FilterValue { Id = 10, Title = "10 параллель" },
-                    new FilterValue { Id = 11, Title = "11 параллель" }
-                }
-            }
-        };
+
         public RelayCommand NavigateCommand { get; }
+
         protected NavCtl()
         {
             Loaded += Page_Loaded;
             this.DataContext = this;
-
-            foreach (MenuFilter filter in Filters)
-                filter.PropertyChanged += Filter_PropertyChanged;
 
             NavigateCommand = new RelayCommand(
                 execute: param =>
@@ -143,49 +77,12 @@ namespace HR.Controls
                 canExecute: param => param is NavigationData
             );
         }
-        private async Task SetFilterData()
-        {
-            IsInProgress = true;
-            // Start both tasks without awaiting immediately
-            var getDptTask = Task.Run(async () =>
-            {
-                using (var ctx = new HR.Data.Models.HREntities())
-                {
-                    return await ctx.Departments.Select(x => new FilterValue { Id = x.Id, Title = x.Title }).ToListAsync();
-                }
-            });
-            var getSubjTask = Task.Run(async () =>
-            {
-                using (var ctx = new HR.Data.Models.HREntities())
-                {
-                    return await ctx.Subjects.Select(x => new FilterValue { Id = x.Id, Title = x.Title }).OrderBy(x => x.Title).ToListAsync();
-                }
-            });
 
-            // Await both tasks in parallel
-            await Task.WhenAll(getDptTask, getSubjTask);
-
-            // Process results
-            //var departments = departmentsTask.Result;
-            //var subjects = subjectsTask.Result;
-
-            IsInProgress = false;
-
-            Filters[1].Values = new ObservableCollection<FilterValue>(getDptTask.Result);
-            Filters[2].Values = new ObservableCollection<FilterValue>(getSubjTask.Result);
-        }
-
-        private void Filter_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-
-        }
-        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        private void Page_Loaded(object sender, RoutedEventArgs e)
         {
             if (MainWindow.frame == null) return;
             MainWindow.frame.Navigated += Nav_Navigated;
-            await SetFilterData();
         }
-
         private void Nav_Navigated(object sender, NavigationEventArgs e)
         {
             // Page name
@@ -193,7 +90,7 @@ namespace HR.Controls
             var navigationParameter = e.ExtraData;
             if (navigationParameter is IEnumerable<FilterValue> selectedFilters)
             {
-                MessageBox.Show("ISE");
+                // Handle collection param here
             }
                 if (navigationParameter is FilterValue filterValue)
             {
