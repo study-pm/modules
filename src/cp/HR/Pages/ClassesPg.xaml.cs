80bc0676a9214b540c8078cb03a22e78ff5168f3
viktor <sterilework@gmail.com>
2025-06-08 14:52:45 +0300

    feat: display classes

--- src/cp/HR/Pages/ClassesPg.xaml.cs
+++ src/cp/HR/Pages/ClassesPg.xaml.cs
@@ -1,9 +1,14 @@
 ﻿using HR.Controls;
+using HR.Data.Models;
+using HR.Models;
 using HR.Utilities;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,13 +26,88 @@ namespace HR.Pages
     /// <summary>
     /// Interaction logic for ClassesPg.xaml
     /// </summary>
-    public partial class ClassesPg : Page
+    public partial class ClassesPg : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string prop = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+
+        public ICommand DeleteItemCommand { get; }
+
         private NavigationService _navigationService;
         public FilterValue FilterParam { get; set; }
+
+        private bool _isInProgress;
+        public bool IsInProgress
+        {
+            get => _isInProgress;
+            set
+            {
+                if (_isInProgress == value) return;
+                _isInProgress = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool? IsListEmpty => Classes?.Count == 0;
+        public bool? IsListNotEmpty => Classes?.Count > 0;
+        private ObservableCollection<ClassGuidance> _classes;
+        public ObservableCollection<ClassGuidance> Classes
+        {
+            get => _classes;
+            set
+            {
+                _classes = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsListEmpty));
+                OnPropertyChanged(nameof(IsListNotEmpty));
+            }
+        }
+        private HR.Data.Models.ClassGuidance _selected;
+        public HR.Data.Models.ClassGuidance Selected
+        {
+            get => _selected;
+            set
+            {
+                _selected = value;
+                OnPropertyChanged();
+                // Update command state on selected user change
+                CommandManager.InvalidateRequerySuggested();
+            }
+        }
         public ClassesPg()
         {
             InitializeComponent();
+            DataContext = this;
+            Classes = new ObservableCollection<HR.Data.Models.ClassGuidance>();
+
+            Loaded += Page_Loaded;
+
+            DeleteItemCommand = new RelayCommand(
+                execute: param =>
+                {
+                    if (param is HR.Data.Models.ClassGuidance toDelete)
+                    {
+                        var result = MessageBox.Show(
+                            $"Вы действительно хотите удалить класс \"{toDelete.Title}\"?",
+                            "Подтверждение удаления",
+                            MessageBoxButton.YesNo,
+                            MessageBoxImage.Warning
+                        );
+
+                        if (result == MessageBoxResult.Yes)
+                        {
+                            Classes.Remove(toDelete);
+                            dataGrid.Items.Refresh(); // refresh auto numbering
+                            // @TODO: Delete from DB
+                            // await Services.Request.DeleteUser(userToDelete.Id);
+                        }
+                    }
+                },
+                canExecute: param =>
+                {
+                    return true;
+                }
+            );
 
             // Subscribe to navigation events for the main frame
             _navigationService = MainWindow.frame.NavigationService;
@@ -36,38 +116,56 @@ namespace HR.Pages
                 _navigationService.Navigated += NavigationService_Navigated;
             }
         }
+        private void DataGrid_LoadingRow(object sender, DataGridRowEventArgs e)
+        {
+            // Numbering from 1
+            e.Row.Header = (e.Row.GetIndex() + 1).ToString();
+        }
+        private void DataGrid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Delete)
+            {
+                var dg = sender as DataGrid;
+                var toDelete = dg?.SelectedItem as HR.Data.Models.ClassGuidance;
+                if (toDelete != null && DeleteItemCommand.CanExecute(toDelete))
+                {
+                    DeleteItemCommand.Execute(toDelete);
+                    e.Handled = true;
+                }
+            }
+        }
         private void NavigationService_Navigated(object sender, NavigationEventArgs e)
         {
             if (e.Content != this) return;
             var navigationParameter = e.ExtraData;
 
-            // Если пришёл один FilterValue — используем его
+            // If there is a single FilterValue — use it
             if (navigationParameter is HR.Utilities.FilterValue filterValue)
             {
                 HandleFilterValue(filterValue);
             }
-            // Если пришла коллекция FilterValue — используем ее
+            // If there is a collection of FilterValues — use it
             if (navigationParameter is IEnumerable<FilterValue> filterValues)
             {
                 HandleFilterValues(filterValues);
             }
-            // Если пришёл NavigationData — извлекаем Parameter
+            // If there is NavigationData — retrieve Parameter
             else if (navigationParameter is NavigationData navData)
             {
-                // Если Parameter — один FilterValue
+                // If Parameter is a single FilterValue
                 if (navData.Parameter is HR.Utilities.FilterValue filter)
                 {
                     HandleFilterValue(filter);
                 }
-                // Если Parameter — коллекция FilterValue
+                // If Parameter is a collection of FilterValues
                 else if (navData.Parameter is IEnumerable<HR.Utilities.FilterValue> filters)
                 {
                     HandleFilterValues(filters);
                 }
                 else
                 {
-                    // Обработка других типов параметров, если нужно
-                    MessageBox.Show("NavigationData.Parameter имеет неожиданный тип: " + navData.Parameter?.GetType().Name);
+                    // Handling other type params, if necessary
+                    MessageBox.Show("NavigationData.Parameter has unexpected type: " + navData.Parameter?.GetType().Name);
                 }
             }
             else if (navigationParameter != null)
@@ -77,23 +175,24 @@ namespace HR.Pages
         }
         private void HandleFilterValue(Utilities.FilterValue filter)
         {
-            // Логика обработки параметра
-            // Например, показать выбранный фильтр в интерфейсе
-            MessageBox.Show($"Выбран класс: {filter.Title}");
+            // Param single value processing logic
         }
 
         private void HandleFilterValues(IEnumerable<FilterValue> filters)
         {
-            // Логика обработки списка выбранных фильтров
-            MessageBox.Show("ClassesPg Filters Change");
+            // Param collection value processing logic
         }
 
-        private void Page_Loaded(object sender, RoutedEventArgs e)
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            if (FilterParam != null)
-            {
-                MessageBox.Show($"Выбран класс: {FilterParam.Title}");
-            }
+            var fromService = await Services.Request.GetClassGuidance();
+            Classes = new ObservableCollection<ClassGuidance>(fromService);
+            // Alternative way: without creating a new instance
+            /*
+            Classes.Clear();
+            foreach (var item in fromService)
+                Classes.Add(item);
+            */
         }
 
         private void Page_Unloaded(object sender, RoutedEventArgs e)
