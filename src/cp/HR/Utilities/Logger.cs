20f7db0ad4f6578dd90b29273c7bbdec0cba633c
viktor <sterilework@gmail.com>
2025-06-12 12:52:44 +0300

    feat: add event logger

---
+++ src/cp/HR/Utilities/Logger.cs
@@ -0,0 +1,132 @@
+﻿using HR.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static HR.Services.AppEventHelper;
+using System.Windows;
+
+namespace HR.Utilities
+{
+    /// <summary>
+    /// Represents a logger that writes events to a JSON log file with filtering support by event categories and types.
+    /// Implements <see cref="IDisposable"/> to properly unsubscribe from events and release resources.
+    /// </summary>
+    public class Logger : IDisposable
+    {
+        /// <summary>
+        /// Directory where log files are stored.
+        /// </summary>
+        public static readonly string logsDir = "Logs";
+        /// <summary>
+        /// The file path of the log file for this logger instance.
+        /// </summary>
+        public string filePath;
+        /// <summary>
+        /// List of event categories that this logger processes.
+        /// </summary>
+        public List<EventCategory> categories;
+        /// <summary>
+        /// List of event types that this logger processes.
+        /// </summary>
+        public List<EventType> types;
+
+        private bool _disposed = false; // Flag to prevent multiple calls to Dispose
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Logger"/> class for a specified user,
+        /// creates the log directory if it does not exist, and subscribes to application events.
+        /// </summary>
+        /// <param name="uid">The unique user identifier used as the log file name.</param>
+        /// <param name="categories">The event categories to filter.</param>
+        /// <param name="types">The event types to filter.</param>
+        public Logger(int uid, List<EventCategory> categories, List<EventType> types)
+        {
+            string logFilePath = System.IO.Path.Combine(logsDir, uid + ".log");
+            string logDirectory = System.IO.Path.GetDirectoryName(logFilePath);
+
+            if (!System.IO.Directory.Exists(logDirectory))
+            {
+                System.IO.Directory.CreateDirectory(logDirectory);
+            }
+            AppEvent += AppEventHelper_AppEvent;
+            this.categories = categories;
+            this.types = types;
+        }
+        /// <summary>
+        /// Releases all resources used by the <see cref="Logger"/>, including unsubscribing from events.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        /// <summary>
+        /// Releases unmanaged and optionally managed resources.
+        /// </summary>
+        /// <param name="disposing">
+        /// <c>true</c> to release both managed and unmanaged resources;
+        /// <c>false</c> to release only unmanaged resources.
+        /// </param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+                return;
+
+            if (disposing)
+            {
+                // Освобождаем управляемые ресурсы
+                AppEvent -= AppEventHelper_AppEvent;
+            }
+
+            // Освобождение неуправляемых ресурсов, если есть
+
+            _disposed = true;
+        }
+        /// <summary>
+        /// Finalizer that releases resources if Dispose was not called.
+        /// </summary>
+        ~Logger()
+        {
+            Dispose(false);
+        }
+        /// <summary>
+        /// Application event handler that logs the event.
+        /// </summary>
+        /// <param name="sender">The event source.</param>
+        /// <param name="e">The event arguments.</param>
+        private void AppEventHelper_AppEvent(object sender, AppEventArgs e)
+        {
+            LogEvent(e, filePath);
+        }
+        /// <summary>
+        /// Logs the specified event to the log file if it matches the configured categories and types.
+        /// If an error occurs during logging, raises an error event.
+        /// </summary>
+        /// <param name="evt">The event to log.</param>
+        /// <param name="fileName">The log file name.</param>
+        public void LogEvent(AppEventArgs evt, string fileName)
+        {
+            try
+            {
+                if (categories.Any(x => x == evt.Category) && types.Any(x => x == evt.Type))
+                {
+                    JsonHelper.AddItem(filePath, evt);
+                }
+            }
+            catch (Exception exc)
+            {
+                AppEventArgs appEvt = new AppEventArgs
+                {
+                    Id = new Guid(),
+                    Timestamp = DateTime.Now,
+                    Category = EventCategory.Service,
+                    Type = EventType.Error,
+                    Message = "Ошибка при сохранении записи в журнал событий",
+                    Details = exc.Message
+                };
+                RaiseAppEvent(appEvt);
+            }
+        }
+    }
+}
