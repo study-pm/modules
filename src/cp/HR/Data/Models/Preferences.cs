7375b925ce23e9ca567fc0bb57d011b1f3925f82
viktor <sterilework@gmail.com>
2025-06-13 17:23:37 +0300

    fix: remove saveasync with id fields, add data annotations

--- src/cp/HR/Data/Models/Preferences.cs
+++ src/cp/HR/Data/Models/Preferences.cs
@@ -10,22 +10,41 @@ using System.Xml.Serialization;
 
 namespace HR.Data.Models
 {
+    [XmlRoot("Preferences")]
     public class Preferences
     {
         public static readonly string basePath = "Preferences";
         public static readonly string prefsFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, basePath);
-        public int UserId {  get; set; }
         public bool IsStayLoggedIn { get; set; }
         public bool IsLogOn { get; set; }
+        [XmlIgnore]
         public HashSet<int> LogCategories { get; set; } = new HashSet<int> { 0, 1, 2, 3 };
+
+        [XmlArray("LogCategories")]
+        [XmlArrayItem("Category")]
+        public List<int> LogCategoriesSerializable
+        {
+            get => LogCategories.ToList();
+            set => LogCategories = new HashSet<int>(value ?? new List<int>());
+        }
+        [XmlIgnore]
         public HashSet<int> LogTypes { get; set; } = new HashSet<int> { 1, 2, 3, 4, 5 };
+
+        [XmlArray("LogTypes")]
+        [XmlArrayItem("Type")]
+        public List<int> LogTypesSerializable
+        {
+            get => LogTypes.ToList();
+            set => LogTypes = new HashSet<int>(value ?? new List<int>());
+        }
+
         private static string GetFilePath(int uid) => System.IO.Path.Combine(prefsFolder, $"{uid.ToString()}.xml");
         public static async Task<Preferences> LoadAsync(int uid)
         {
             string filePath = GetFilePath(uid);
             if (!System.IO.File.Exists(filePath))
             {
-                return new Preferences { UserId = uid, IsStayLoggedIn = false };
+                return new Preferences();
             }
             return await Task.Run(() =>
             {
@@ -40,32 +59,7 @@ namespace HR.Data.Models
                 catch(Exception exc)
                 {
                     Debug.WriteLine(exc.Message);
-                    return new Preferences { IsStayLoggedIn = false };
-                }
-            });
-        }
-
-        public async Task SaveAsync()
-        {
-            await Task.Run(() =>
-            {
-                try
-                {
-                    var filePath = GetFilePath(UserId);
-                    var directory = System.IO.Path.GetDirectoryName(filePath);
-                    if (!System.IO.Directory.Exists(directory))
-                        System.IO.Directory.CreateDirectory(directory);
-
-                    var serializer = new XmlSerializer(typeof(Preferences));
-                    using (var stream = System.IO.File.Create(filePath))
-                    {
-                        serializer.Serialize(stream, this);
-                    }
-                }
-                catch (Exception exc)
-                {
-                    Debug.WriteLine(exc.Message);
-                    throw exc;
+                    return new Preferences();
                 }
             });
         }
