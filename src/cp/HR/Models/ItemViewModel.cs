42c64d2f6936072ee8e65c74b510766b3d7f7f4c
viktor <sterilework@gmail.com>
2025-06-14 03:27:38 +0300

    feat: add collection support to copy and check is changed methods

--- src/cp/HR/Models/ItemViewModel.cs
+++ src/cp/HR/Models/ItemViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using HR.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -77,6 +78,52 @@ namespace HR.Models
             };
         }
         /// <summary>
+        /// Determines whether the given pair of types represent a collection pair consisting of a List and an ObservableCollection, in either order.
+        /// </summary>
+        /// <param name="type1">The first type to check.</param>
+        /// <param name="type2">The second type to check.</param>
+        /// <returns><c>true</c> if one type is a generic List and the other is a generic ObservableCollection; otherwise, <c>false</c>.</returns>
+        private bool IsCollectionPair(Type type1, Type type2)
+        {
+            return (IsGenericType(type1, typeof(List<>)) && IsGenericType(type2, typeof(ObservableCollection<>))) ||
+                   (IsGenericType(type1, typeof(ObservableCollection<>)) && IsGenericType(type2, typeof(List<>)));
+        }
+        /// <summary>
+        /// Determines whether a given type is a generic type of a specified generic type definition.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        /// <param name="genericType">The generic type definition to compare against (e.g., typeof(List<>)).</param>
+        /// <returns><c>true</c> if <paramref name="type"/> is a generic type and its generic type definition matches <paramref name="genericType"/>; otherwise, <c>false</c>.</returns>
+        private bool IsGenericType(Type type, Type genericType)
+        {
+            return type.IsGenericType && type.GetGenericTypeDefinition() == genericType;
+        }
+        /// <summary>
+        /// Compares two collections to determine if they contain different elements, regardless of order and duplicates.
+        /// </summary>
+        /// <param name="first">The first collection to compare.</param>
+        /// <param name="second">The second collection to compare.</param>
+        /// <returns><c>true</c> if the collections are different (different counts or different sets of elements); otherwise, <c>false</c>.</returns>
+        private bool AreCollectionsDifferent(System.Collections.IEnumerable first, System.Collections.IEnumerable second)
+        {
+            if (ReferenceEquals(first, second))
+                return false;
+
+            if (first == null || second == null)
+                return true;
+
+            var firstList = first.Cast<object>().ToList();
+            var secondList = second.Cast<object>().ToList();
+
+            if (firstList.Count != secondList.Count)
+                return true;
+
+            var firstSet = new HashSet<object>(firstList);
+            var secondSet = new HashSet<object>(secondList);
+
+            return !firstSet.SetEquals(secondSet);
+        }
+        /// <summary>
         /// Checks whether any property value in the editable model <see cref="Dm"/> differs from the corresponding property in the original model <see cref="_dm"/>.
         /// Compares all readable public instance properties that exist in both types by name.
         /// </summary>
@@ -107,6 +154,16 @@ namespace HR.Models
                 if (originalValue == null || currentValue == null)
                     return true;
 
+                // Check if props are List<T> and ObservableCollection<T> collections
+                if (IsCollectionPair(originalValue.GetType(), currentValue.GetType()))
+                {
+                    if (AreCollectionsDifferent(originalValue as System.Collections.IEnumerable, currentValue as System.Collections.IEnumerable))
+                        return true;
+                    else
+                        continue;
+                }
+
+                // Compare with equals only if not collections
                 if (!originalValue.Equals(currentValue))
                     return true;
             }
@@ -135,12 +192,59 @@ namespace HR.Models
                 if (targetProp == null || !targetProp.CanWrite)
                     continue;
 
-                if (!targetProp.PropertyType.IsAssignableFrom(sourceProp.PropertyType))
+                var sourceValue = sourceProp.GetValue(source);
+                if (sourceValue == null)
+                {
+                    targetProp.SetValue(target, null);
                     continue;
+                }
 
-                var value = sourceProp.GetValue(source);
-                targetProp.SetValue(target, value);
+                // If types match and targetProp can get sourceProp directly
+                if (targetProp.PropertyType.IsAssignableFrom(sourceProp.PropertyType))
+                {
+                    targetProp.SetValue(target, sourceValue);
+                    continue;
+                }
+
+                // Copy from List<T> to ObservableCollection<T>
+                if (IsGenericType(sourceProp.PropertyType, typeof(List<>)) &&
+                    IsGenericType(targetProp.PropertyType, typeof(ObservableCollection<>)))
+                {
+                    var itemType = sourceProp.PropertyType.GetGenericArguments()[0];
+                    var list = (System.Collections.IEnumerable)sourceValue;
+
+                    var observableCollectionType = typeof(ObservableCollection<>).MakeGenericType(itemType);
+                    var newObservableCollection = Activator.CreateInstance(observableCollectionType, list);
+
+                    targetProp.SetValue(target, newObservableCollection);
+                    continue;
+                }
+
+                // Copy from ObservableCollection<T> to List<T>
+                if (IsGenericType(sourceProp.PropertyType, typeof(ObservableCollection<>)) &&
+                    IsGenericType(targetProp.PropertyType, typeof(List<>)))
+                {
+                    var itemType = sourceProp.PropertyType.GetGenericArguments()[0];
+                    var observableCollection = (System.Collections.IEnumerable)sourceValue;
+
+                    var listType = typeof(List<>).MakeGenericType(itemType);
+                    var newList = Activator.CreateInstance(listType, observableCollection);
+
+                    targetProp.SetValue(target, newList);
+                    continue;
+                }
             }
+        }
+        /// <summary>
+        /// Creates a new instance of type <typeparamref name="T"/> and copies the properties from the <c>Dm</c> object to this new instance.
+        /// </summary>
+        /// <typeparam name="T">The type of the object to create and copy properties to. Must have a parameterless constructor.</typeparam>
+        /// <returns>A new instance of <typeparamref name="T"/> with properties copied from <c>Dm</c>.</returns>
+        public T Preset()
+        {
+            var preset = new T();
+            CopyProperties(Dm, preset);
+            return preset;
         }
         /// <summary>
         /// Resets the editable model <see cref="Dm"/> by copying all property values from the original model <see cref="_dm"/>.
