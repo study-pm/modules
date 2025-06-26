c0c427a74881af0c4bae63e139702535fa08f2eb
viktor <sterilework@gmail.com>
2025-06-26 14:08:08 +0300

    feat: add startup page and user app state model classes

---
+++ src/cp/HR/Data/Models/StartupPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Data.Models
+{
+    public class StartupPage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string Uri { get; set; }
+    }
+}
---
+++ src/cp/HR/Data/Models/UserAppState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Data.Models
+{
+    [Serializable]
+    public class UserAppState
+    {
+        public static readonly string basePath = "Cache";
+        public static readonly string cacheFolder = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, basePath);
+        public string LastState { get; set; }
+    }
+}
