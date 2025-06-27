2e7a83255410a0b97c79271cb5c73d30f5ca8644
viktor <sterilework@gmail.com>
2025-06-27 17:12:06 +0300

    feat: add main education title to model

--- src/cp/HR/Data/Models/Employee.partial.cs
+++ src/cp/HR/Data/Models/Employee.partial.cs
@@ -39,6 +39,7 @@ namespace HR.Data.Models
         {
             get => string.Join(" ", new[] { Surname, GivenName, Patronymic }.Where(s => !string.IsNullOrWhiteSpace(s)));
         }
+        public string MainEducationTitle => Educations?.FirstOrDefault()?.Qualification?.Title ?? string.Empty;
         public string MainPositionTitle => Staffs?.FirstOrDefault()?.Position?.Title ?? string.Empty;
         public IEnumerable<Subject> Subjects
         {
