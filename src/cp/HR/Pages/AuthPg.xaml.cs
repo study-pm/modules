478782728de50e334c70591f453e980c801002d0
viktor <sterilework@gmail.com>
2025-07-05 23:53:58 +0300

    feat: add 2fa second step check support

--- src/cp/HR/Pages/AuthPg.xaml.cs
+++ src/cp/HR/Pages/AuthPg.xaml.cs
@@ -2,6 +2,7 @@
 using HR.Models;
 using HR.Services;
 using HR.Utilities;
+using OtpNet;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,6 +10,8 @@ using System.Data.Entity;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
+using System.Net.Sockets;
+using System.Security;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -53,6 +56,17 @@ namespace HR.Pages
         protected void OnPropertyChanged(string propertyName) =>
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 
+        private string _code;
+        public string Code
+        {
+            get => _code;
+            set
+            {
+                if (_code == value) return;
+                _code = value;
+                OnPropertyChanged(nameof(Code));
+            }
+        }
         private bool _isPwdOn;
         public bool IsPwdOn
         {
@@ -81,10 +95,82 @@ namespace HR.Pages
                 ValidatePassword();
             }
         }
+        private bool _isSecondStep;
+        public bool IsSecondStep
+        {
+            get => _isSecondStep;
+            set
+            {
+                if (_isSecondStep == value) return;
+                _isSecondStep = value;
+                OnPropertyChanged(nameof(IsSecondStep));
+            }
+        }
+        private string secret;
+        public string Secret
+        {
+            get => secret;
+            set
+            {
+                if (secret == value) return;
+                secret = value;
+                OnPropertyChanged(nameof(Secret));
+            }
+        }
         public AuthPg()
         {
             InitializeComponent();
             DataContext = this;
+        }
+        private async Task<string> GetDecryptedSecret(string cipherText)
+        {
+            var (cat, name, op, scope) = (EventCategory.Auth, "Read", 1, "Аутентификация");
+            try
+            {
+                IsInProgress = true;
+                RaiseAppEvent(new AppEventArgs
+                {
+                    Category = cat,
+                    Name = name,
+                    Op = op,
+                    Scope = scope,
+                    Type = EventType.Progress,
+                    Message = "Дешифровка данных"
+                });
+                string basePath = Fs.GetFullRootPath(Crypto.keysPath);
+                var (key, iv) = await Fs.LoadKeysParallelAsync(basePath, "aes_key.bin", "aes_iv.bin");
+                string decrypted = Crypto.Decrypt(cipherText, key, iv);
+                RaiseAppEvent(new AppEventArgs
+                {
+                    Category = cat,
+                    Name = name,
+                    Op = op,
+                    Scope = scope,
+                    Type = EventType.Success,
+                    Message = "Данные успешно дешифрованы"
+                });
+                return decrypted;
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine(exc, "AuthPg");
+                RaiseAppEvent(new AppEventArgs
+                {
+                    Category = cat,
+                    Name = name,
+                    Op = op,
+                    Scope = scope,
+                    Type = EventType.Error,
+                    Message = "Ошибка дешифровки данных",
+                    Details = exc.Message
+                });
+                MessageBox.Show($"Ошибка дешифровки данных: {exc.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return "";
+            }
+            finally
+            {
+                IsInProgress = false;
+            }
         }
         private async Task<HR.Data.Models.User> GetUser(string login, string password)
         {
@@ -119,6 +205,31 @@ namespace HR.Pages
                 return null;
             }
         }
+        private async Task LogIn()
+        {
+            App app = Application.Current as App;
+            // Set current user
+            app.CurrentUser = await GetUser(Login, Password);
+            // Read and apply user preferences
+            Preferences preferences = await Services.Request.GetPreferences(app.CurrentUser.Id);
+            app.Preferences = preferences;
+            // Manage user file
+            if (preferences.IsStayLoggedIn) await Services.Request.SaveUidToFileAsync(app.CurrentUser.Id, Data.Models.User.uidFilePath);
+            // Manage logging
+            if (preferences.IsLogOn) App.Current.EventLogger = new Logger(app.CurrentUser.Id, preferences.LogCategories, preferences.LogTypes);
+            RaiseAppEvent(new AppEventArgs
+            {
+                Category = EventCategory.Auth,
+                Type = EventType.Info,
+                Name = "Login",
+                Op = 2,
+                Scope = "Приложение",
+                Message = "Вход в систему",
+                Details = "Пользовательский режим (новый сеанс)"
+            });
+            // @TODO: Go to Startup page
+            if (app.IsAuth == true) MainWindow.frame.Navigate(new PreferencesPg());
+        }
         private void ValidatePassword()
         {
             var rule = new NotEmptyValidationRule();
@@ -136,6 +247,45 @@ namespace HR.Pages
                     ErrorContent = result.ErrorContent
                 };
                 Validation.MarkInvalid(bindingExpression, validationError);
+            }
+        }
+
+        private async void CodeTxb_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox txb = (TextBox)sender;
+            if (Validation.GetHasError(txb)) return;
+
+            try
+            {
+                var secretBytes = Base32Encoding.ToBytes(await GetDecryptedSecret(Secret));
+                var totp = new Totp(secretBytes);
+                bool isValid = totp.VerifyTotp(Code, out long timeStepMatched, new VerificationWindow(2, 2));
+                if (isValid)
+                {
+                    await LogIn();
+                }
+                else
+                {
+                    StatusInformer.ReportFailure($"Ошибка проверки кода регистрации 2FA");
+                    MessageBox.Show("Ошибка проверки кода двухфакторной аутентификации.\n\n" +
+                        "Введённый код неверен. Проверьте код из приложения Google Authenticator и попробуйте снова.\n\n" +
+                        "• Убедитесь, что время на вашем устройстве синхронизировано и установлено правильно.\n" +
+                        "• Введите актуальный шестизначный код, который обновляется каждые 30 секунд.\n" +
+                        "• Если проблема сохраняется, попробуйте заново отсканировать QR-код и повторить регистрацию.",
+                        "Ошибка 2FA",
+                        MessageBoxButton.OK,
+                        MessageBoxImage.Error);
+                }
+            }
+            catch (Exception exc)
+            {
+                StatusInformer.ReportFailure($"Непредвиденная ошибка при проверке кода регистрации 2FA: {exc.Message.ToString()}");
+                MessageBox.Show("Произошла непредвиденная ошибка при проверке кода: " + exc.Message.ToString() +
+                    "Попробуйте повторить попытку позже.\n\n" +
+                    "Если проблема сохраняется, обратитесь в службу поддержки для получения помощи.",
+                    "Ошибка 2FA",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
             }
         }
         public void ResetFields()
@@ -224,35 +374,26 @@ namespace HR.Pages
                 return;
             }
 
-            // 4. Если ошибок нет - выполняем вход
+            // 4. Если ошибок нет, проверяем пользователя, 2FA и выполняем вход
             IsInProgress = true;
             StatusInformer.ReportProgress("Проверка данных пользователя");
             App app = Application.Current as App;
-            app.CurrentUser = await GetUser(Login, Password);
-            if (!app.IsAuth)
+            HR.Data.Models.User user = await GetUser(Login, Password);
+            if (user == null)
             {
                 IsInProgress = false;
                 return;
             }
-            // Read and apply user preferences
-            Preferences preferences = await Services.Request.GetPreferences(app.CurrentUser.Id);
-            app.Preferences = preferences;
-            // Manage user file
-            if (preferences.IsStayLoggedIn) await Services.Request.SaveUidToFileAsync(app.CurrentUser.Id, Data.Models.User.uidFilePath);
-            // Manage logging
-            if (preferences.IsLogOn) App.Current.EventLogger = new Logger(app.CurrentUser.Id, preferences.LogCategories, preferences.LogTypes);
-            RaiseAppEvent(new AppEventArgs {
-                Category = EventCategory.Auth,
-                Type = EventType.Info,
-                Name = "Login",
-                Op = 2,
-                Scope = "Приложение",
-                Message = "Вход в систему",
-                Details = "Пользовательский режим (новый сеанс)"
-            });
+            if (user.Is2faOn)
+            {
+                IsSecondStep = true;
+                Secret = user.Secret;
+            }
+            else
+            {
+                await LogIn();
+            }
             IsInProgress = false;
-            // @TODO: Go to Startup page
-            if (app.IsAuth == true) MainWindow.frame.Navigate(new PreferencesPg());
         }
         private void ResetBtn_Click(object sender, RoutedEventArgs e)
         {
